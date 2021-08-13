1492dbf9fa6e524c06321e397c7746ea321eba52
gaurav14rana <68178302+gaurav14rana@users.noreply.github.com>
2021-08-13 22:27:49 +0530

    Add files via upload

---
+++ circlearea.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Code for Circle Area
+"""
+cirRadius = float(input("Enter the radius of the circle:"))
+piValue = 3.14
+cirArea = piValue * cirRadius * cirRadius
+print("The area of the circle with radius", cirRadius, "is: ", cirArea)
+
